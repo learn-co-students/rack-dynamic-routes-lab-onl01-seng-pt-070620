829f8af66d1e473c82c3673a59c6b1de699a9cdc
Keesha Hargrove <kharg14@gmail.com>
2020-09-22 22:39:06 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application 
+    @@items = [Item.new("Figs", 3.42),
+               Item.new("Pears", 0.99)]
+    def call(env)
+        req = Rack::Request.new(env)
+        resp = Rack::Response.new
+
+        if req.path.match('/items/') 
+            item_name = req.path.split("/items/").last
+            item = @@items.find do |i|
+                if i.name == item_name
+                    resp.write i.price
+                else
+                    resp.status = 400
+                    resp.write "Item not found"
+                end
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+end 
