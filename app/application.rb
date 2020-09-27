d039e7794be28e70f24bcfbd1981b142726d9a5b
jmd <jordan.michael.dickey@gmail.com>
2020-09-27 14:31:27 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,6 @@
 
 class Application
  
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -25,18 +24,3 @@ class Application
   end
 end
 
-# def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
- 
-#     if req.path.match(/songs/)
- 
-#       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-#       song = @@songs.find{|s| s.title == song_title}
- 
-#       resp.write song.artist
-#     end
- 
-#     resp.finish
-#   end
-# end
